107e7b4c279dbf77a6fe8d438a608c588611fac2
Judy Tuan <judy@indiegogo.com>
2016-06-02 16:59:45 -0700

    Template expression via interpolation, property binding, event binding, two-way binding, local variable etc

--- src/app/+gadgets/gadgets.component.ts
+++ src/app/+gadgets/gadgets.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GadgetsComponent implements OnInit {
   body: string = 'We are on gadgets now.';
+  title: string = 'Gadgets';
+  color: string = 'green';
+  text: string = 'initial value';
+
+  changeColor() {
+    const colors = ['red', 'goldenrod', 'orchid'];
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  }
 
   constructor() {
 
@@ -17,3 +25,6 @@ export class GadgetsComponent implements OnInit {
   }
 
 }
+
+
+// https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { ROUTER_PROVIDERS } from '@angular/router';
+//import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
